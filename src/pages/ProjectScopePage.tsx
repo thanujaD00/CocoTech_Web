e0c34bcac22b31201d60e112377fef803d4bf8b7
sadeeparuwanpura <sadeepa.ruwanpura@layoutindex.lk>
2025-05-25 14:12:06 +0530

    update

--- src/pages/ProjectScopePage.tsx
+++ src/pages/ProjectScopePage.tsx
@@ -762,7 +762,7 @@ const ProjectScopePage = () => {
                 <motion.a
                   whileHover={{ scale: 1.05 }}
                   transition={{ type: "spring", stiffness: 400 }}
-                  href="/downloads"
+                  href="https://drive.google.com/drive/folders/17pqbxGbR-Wjdw7-Srur-LpM2W3Zzwh8j?usp=drive_link"
                   className="border border-green-700 text-green-700 px-6 py-3 rounded-md text-center hover:bg-green-50 transition-colors"
                 >
                   Download Research Paper
